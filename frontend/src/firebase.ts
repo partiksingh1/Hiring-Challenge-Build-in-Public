1dcf224f407ea65870608291d7c2ac67cdfe22e5
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2025-03-14 23:30:22 +0530

    configs

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -6,14 +6,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBYj5gPF9SnAbVUzPJUTCSwTrmkCfPkSbY",
-    authDomain: "balance-it-a7104.firebaseapp.com",
-    projectId: "balance-it-a7104",
-    storageBucket: "balance-it-a7104.firebasestorage.app",
-    messagingSenderId: "630956322456",
-    appId: "1:630956322456:web:90c1deb0b3b67ecff32aed",
-    measurementId: "G-H1VDWZF0DG"
-  };
+  apiKey: "AIzaSyAD8XhF7foCf3tSRiZRLi3zvWT1MaLrJZc",
+  authDomain: "balancescale-4074d.firebaseapp.com",
+  projectId: "balancescale-4074d",
+  storageBucket: "balancescale-4074d.firebasestorage.app",
+  messagingSenderId: "787730771629",
+  appId: "1:787730771629:web:109e29b2df474c3de7a024",
+  measurementId: "G-JZ8TW4FE77"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
